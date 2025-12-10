744720b47fde99b5d5cd8b9bac8c3f347b11f49e
eligoze <eligoze75@gmail.com>
2025-12-09 20:46:06 -0800

    Saving checkpoint for testing purposes

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import numpy as np
 
-def custom_train_test_split(dataframe: pd.DataFrame, cat_col: str, test_size: float = 0.3, random_state: int = 573):
+
+def custom_train_test_split(
+    dataframe: pd.DataFrame,
+    cat_col: str,
+    test_size: float = 0.3,
+    random_state: int = 573,
+):
     np.random.seed(random_state)
 
     df: pd.DataFrame = dataframe.copy()
 
     cat_ids: np.ndarray = df[cat_col].unique()
 
-    random_shuffling: dict = {cat:np.random.rand() for cat in cat_ids}
+    random_shuffling: dict = {cat: np.random.rand() for cat in cat_ids}
 
     df["ordered_cat"] = df[cat_col].map(random_shuffling)
 
@@ -21,7 +27,7 @@ def custom_train_test_split(dataframe: pd.DataFrame, cat_col: str, test_size: fl
         if cum_count / len(df) < 1 - test_size:
             cutoff = cat
             break
-            
+
     df = df.drop("ordered_cat", axis=1).set_index(cat_col)
 
     train_df = df.loc[:cutoff].reset_index()
